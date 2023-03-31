79cdab41c4605aea500879afda5068ba93da108f
IPG-work <ivanin.pg@gmail.com>
2023-03-31 19:19:53 +0300

    * создана база данных

--- chat/models.py
+++ chat/models.py
@@ -4,9 +4,12 @@ from django.db import models
 # Create your models here.
 
 class Member(models.Model):
-    memberUser = models.OneToOneField(User, on_delete=models.CASCADE)
+    memberUser = models.OneToOneField(User, primary_key=True, on_delete=models.CASCADE)
+    icon = models.ImageField(upload_to='icon/')
 
 
 class Chat(models.Model):
     name = models.CharField(max_length=64)
-    member = models.ManyToManyField(Member)
+    members = models.ManyToManyField(Member)
+
+    def __init__(self):
